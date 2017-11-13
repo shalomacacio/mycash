2e284d271f24abf8e21a0051bc74f5826e34431e
Werbyston Lemos <estagio.ti@fak.edu.br>
2017-11-13 14:22:20 -0200

    reorganização do form produto

--- resources/views/produto/_form_produto.blade.php
+++ resources/views/produto/_form_produto.blade.php
@@ -1,6 +1,28 @@
 
 
 <div class="form-group col-xs-4">
+	{!! Form::label('categoria_id','Categoria') !!}
+
+	<div class="input-group">
+                <div class="input-group-btn">
+                   <a href="#" class="btn btn-info" role="button" data-toggle="modal" data-target="#modal-categoria"><i class="fa fa-plus"></i></a>
+                </div>
+	{!! Form::select('categoria_id',isset($produto->categoria)?$produto->categoria->pluck('descricao', 'id'): $categorias,  isset($produto->categoria_id)? $produto->categoria_id : null , ['class'=>'form-control', 'required', 'placeholder' => 'CATEGORIA' ]) !!}
+	</div>
+</div>
+
+<div class="form-group col-xs-4">
+	{!! Form::label('marca_id','Marca') !!}
+
+	<div class="input-group">
+                <div class="input-group-btn">
+                   <a href="#" class="btn btn-info" role="button"  data-toggle="modal" data-target="#modal-marca" ><i class="fa fa-plus"></i></a>
+                </div>
+	{!! Form::select('marca_id',isset($produto->marca)?$produto->marca->pluck('descricao', 'id'): $marcas ,  isset($produto->marca_id)? $produto->marca_id : null , ['class'=>'form-control', 'required', 'placeholder' => 'MARCA' ]) !!}
+	</div>
+</div>
+
+<div class="form-group col-xs-4">
 	{!! Form::label('codigo_interno','Código') !!}
 	<div class="input-group">
                 <div class="input-group-btn">
@@ -19,29 +41,6 @@
 	{!! Form::label('descricao','Descrição') !!}
 	{!! Form::text('descricao',isset($produto->descricao)? $produto->descricao : null , ['class'=>'form-control', 'required', 'placeholder' => 'DESCRICAO' ]) !!}
 </div>
-
-<div class="form-group col-xs-4">
-	{!! Form::label('marca_id','Marca') !!}
-
-	<div class="input-group">
-                <div class="input-group-btn">
-                   <a href="#" class="btn btn-info" role="button"  data-toggle="modal" data-target="#modal-marca" ><i class="fa fa-plus"></i></a>
-                </div>
-	{!! Form::select('marca_id',isset($produto->marca)?$produto->marca->pluck('descricao', 'id'): $marcas ,  isset($produto->marca_id)? $produto->marca_id : null , ['class'=>'form-control', 'required', 'placeholder' => 'MARCA' ]) !!}
-	</div>
-</div>
-
-<div class="form-group col-xs-4">
-	{!! Form::label('categoria_id','Categoria') !!}
-
-	<div class="input-group">
-                <div class="input-group-btn">
-                   <a href="#" class="btn btn-info" role="button" data-toggle="modal" data-target="#modal-categoria"><i class="fa fa-plus"></i></a>
-                </div>
-	{!! Form::select('categoria_id',isset($produto->categoria)?$produto->categoria->pluck('descricao', 'id'): $categorias,  isset($produto->categoria_id)? $produto->categoria_id : null , ['class'=>'form-control', 'required', 'placeholder' => 'CATEGORIA' ]) !!}
-	</div>
-</div>
-
 
 <div class="form-group col-xs-4">
 	{!! Form::label('preco_venda','Preço Venda') !!}
