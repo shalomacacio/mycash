4e2486bf68d47ded51d0ca3a8b064661184b00e7
Werbyston Lemos <estagio.ti@fak.edu.br>
2018-03-13 19:32:52 -0300

    view finalizar venda

--- resources/views/venda/_form_fin_venda.blade.php
+++ resources/views/venda/_form_fin_venda.blade.php
@@ -1,20 +1,29 @@
 
 
 <div class="form-group col-xs-12 col-md-4">
+	{!! Form::label('cliente','Cliente') !!}
+	{!! Form::text('cliente', null , ['class'=>'form-control' ]) !!}
+</div>
+
+<div class="form-group col-xs-12 col-md-2">
 	{!! Form::label('tipo_pagamento','Tipo de Pagamento') !!}
-	{!! Form::select('produto_id', array('AV' => 'A VISTA ', 'CC' => 'CREDITO', 'CD' => 'DEBITO'), null,   ['class'=>'form-control select2' , 'data-placeholder' => 'Selecione...'] ) !!}
+	{!! Form::select('tipo_pagamento', array('V' => 'A VISTA ', 'C' => 'CREDITO', 'D' => 'DEBITO'), null,   ['class'=>'form-control select2' , 'data-placeholder' => 'Selecione...'] ) !!}
 	
 </div>
 
-<div class="form-group col-xs-12 col-md-4">
-	{!! Form::label('parcelas','N Parcelas') !!}
-	{!! Form::text('parcelas', null , ['class'=>'form-control' ]) !!}
+<div class="form-group col-xs-12 col-md-2">
+	{!! Form::label('num_parcelas','N Parcelas') !!}
+	{!! Form::text('num_parcelas', null , ['class'=>'form-control' ]) !!}
 </div>
 
-<div class="form-group col-xs-12 col-md-4">
+<div class="form-group col-xs-12 col-md-2">
 	{!! Form::label('desconto','Desconto') !!}
 	{!! Form::text('desconto', null , ['class'=>'form-control' ]) !!}
 </div>
 
+<div class="form-group col-xs-12 col-md-2">
+	{!! Form::label('total_geral','Total') !!}
+	{!! Form::text('total_geral', $venda->produtos()->sum('subtotal') , ['class'=>'form-control', 'readOnly' ]) !!}
+</div>
 
 {!! Form::hidden('id', $venda->id ) !!}
--- resources/views/venda/finalizar_venda.blade.php
+++ resources/views/venda/finalizar_venda.blade.php
@@ -43,94 +43,21 @@
 
 <script type="text/javascript">
 
-    $('#btn_add').on('click', function (e) {
-        e.preventDefault();
-  
-        var codigo = $('#codigo').val();
-        var lote_id = $('#lote_id').val();
-        var fornecedor_id = $('#fornecedor_id').val();
-        var num_pedido = $('#num_pedido').val();
-        var produto_id = $('#produto_id').val();
-        var preco_compra = $('#preco_compra').val();
-        var qtd = $('#qtd').val();
-        var subtotal = $('#subtotal').val();
 
-        $.ajax({
-            headers: { 'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content') },
-            type: "POST",
-            url: '/compra/addItem',
-            data: {codigo: codigo, lote_id: lote_id, fornecedor_id: fornecedor_id, num_pedido: num_pedido, produto_id: produto_id, preco_compra: preco_compra, qtd: qtd , subtotal: subtotal},
-            dataType: 'JSON',
-            success: function( itens ) {
-                  //limpar tabela 
-                  $('#itens-table').empty();
-                  $.each(itens, function(key, data){
-                  $('#itens-table').append('<tr id=row'+key+'>'+
-                                            '<td id=proid'+ key+' visible="false">'+ data.produto_id +'</td>'+
-                                            '<td id=nom'+ key+'>'+ data.nome +'</td>'+
-                                            '<td id=pre'+ key+'>'+data.preco_compra+'</td>'+
-                                            '<td id=qtd'+ key+'>'+data.qtd+'</td>'+
-                                            '<td id=sub'+ key+'>'+data.subtotal+'</td>'+
-                                            '<td><a  type="button" class="btn btn-danger btn_remove"  id="'+key +'"><i class="fa fa-trash"></i></a></td>'+
-                                            '</tr>'); 
-                });
-            }
-            
-        });
-        limpaCampos();
-        
-    });
+  $('input[name=desconto]').blur(function() {
 
-    $(document).on('click', '.btn_remove', function(){
-        var button_id = $(this).attr("id");
-        var codigo = $('#codigo').val();
-        var produto_id = document.getElementById("proid"+button_id).firstChild.nodeValue;
-        //var produto = document.getElementById("nom"+currentId);
-        //codigo.firstChild.nodeValue
-        //alert(produto_id);
-        $.ajax({
-           headers: { 'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content') },
-           type: "POST",
-           url: '/compra/delItem/',
-           data:{produto_id: produto_id, codigo: codigo }, 
-           dataType: 'JSON', 
-           success: function(itens2){
-            //console.log(itens2);
-            $('#itens-table').empty();
-                  $.each(itens2, function(key, data){
-                  $('#itens-table').append(
-                    '<tbody>'+
-                    '<td id=proid'+ key+' visible="false">'+ data.produto_id +'</td>'+
-                    '<td id=nom'+ key+'>'+ data.nome +'</td>'+
-                    '<td id=pre'+ key+'>'+data.preco_compra+'</td>'+
-                    '<td id=qtd'+ key+'>'+data.qtd+'</td>'+
-                    '<td id=sub'+ key+'>'+data.subtotal+'</td>'+
-                    '<td><a  type="button" class="btn btn-danger btn_remove"  id="'+key +'"><i class="fa fa-trash"></i></a></td>'+
-                    '</tbody>'); 
-                  });
-           }
-        });
-        
-    });
+    
 
+    var desconto = parseInt($('input[name=desconto]').val());
+    var total = parseInt($('input[name=total_geral]').val());  
 
-      $('input[name=qtd]').blur(function() {
+     alert(desconto);
 
-        //var $vlrVenda = $("#vlr_venda").val();
-        var qtd = parseInt($('input[name=qtd]').val());
-        var preco_compra = parseFloat ($('input[name=preco_compra]').val());
-        var subtotal = qtd * preco_compra;       
-        parseFloat ($('input[name=subtotal]').val(subtotal.toFixed(2)));
-       });
+    //var totalGeral = total - desconto;
+   // parseInt($('input[name=total_geral]').val(totalGeral.toFixed(2)));
+    parseFloat ($('input[name=total_geral]').val(totalGeral.toFixed(2)));
 
-      function limpaCampos(){
-        $('input[name=qtd]').val('');
-        $('input[name=preco_compra]').val('');
-        $('input[name=subtotal]').val('');
-      }
-
-
-
+  });
 </script>
 
 @endpush
